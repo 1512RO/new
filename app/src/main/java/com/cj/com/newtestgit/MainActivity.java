f07780a986dc22c138e392677dbe5876837f81db
caojun_caojun@126.com <caojun_caojun@126.com>
2018-04-11 11:37:28 +0800

    add

--- app/src/main/java/com/cj/com/newtestgit/MainActivity.java
+++ app/src/main/java/com/cj/com/newtestgit/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e("message","caojun1");
         Log.e("message","weihuanying1");
+        Log.e("myMessage","master");
     }
 }
